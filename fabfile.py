8bd1999f8d006546234532c313ac32d915380e23
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2014-01-09 08:02:51 -0600

    integración del fabfile para deploy

---
+++ fabfile.py
@@ -0,0 +1,19 @@
+import os
+from fabric.api import local, settings, abort, run, env
+from fabric.operations import put
+
+USER = 'developingo'
+HOST = 'developingo.webfactional.com'
+
+REMOTE_PATH = '/home/developingo/webapps/bittsys/snake'
+
+env.hosts = ['%s@%s' % (USER, HOST)]
+
+def deploy():
+    """Do the magic"""
+    local('make clean')
+    local('make')
+
+    put(local_path='dist/index.html', remote_path=os.path.join(REMOTE_PATH, 'index.html'))
+    put(local_path='dist/package.manifest', remote_path=os.path.join(REMOTE_PATH, 'package.manifest'))
+    put(local_path='dist/package.zip', remote_path=os.path.join(REMOTE_PATH, 'package.zip'))
